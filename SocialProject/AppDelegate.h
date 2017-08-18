dfaefda7556fe872b0aeb72e7a842082bc20eac3
Yantzzz <ty2735600@gmail.com>
2017-08-17 20:35:18 -0700

    Initial Commit

---
+++ SocialProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocialProject
+//
+//  Created by Yan Tian on 8/17/17.
+//  Copyright © 2017 Yan Tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SocialProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocialProject
+//
+//  Created by Yan Tian on 8/17/17.
+//  Copyright © 2017 Yan Tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
